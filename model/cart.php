f25355ceb8f172030b49b3fa0eb2304ea74d4e2f
王文韜 <oubun@oubunnoMacBook-Pro.local>
2021-04-29 19:29:03 +0900

    SQLインジェクション

--- model/cart.php
+++ model/cart.php
@@ -70,7 +70,7 @@ function insert_cart($db, $user_id, $item_id, $amount = 1){
         user_id,
         amount
       )
-    VALUES({$item_id}, {$user_id}, {$amount})
+    VALUES(?, ?, ?)
   ";
 
   return execute_query($db, $sql);
--- view/templates/messages.php
+++ view/templates/messages.php
@@ -1,6 +1,6 @@
 <?php foreach(get_errors() as $error){ ?>
-  <p class="alert alert-danger"><span><?php echo h $error; ?></span></p>
+  <p class="alert alert-danger"><span><?php echo h($error); ?></span></p>
 <?php } ?>
 <?php foreach(get_messages() as $message){ ?>
-  <p class="alert alert-success"><span><?php echo h $message; ?></span></p>
+  <p class="alert alert-success"><span><?php echo h($message); ?></span></p>
 <?php } ?>
